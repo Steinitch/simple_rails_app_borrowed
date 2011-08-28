64318976574be8e1cd574f7e802e2628b6d6842e
Mihause <mihause@ngs.ru>
2011-08-29 02:38:00 +0700

    Protecting edit and update with a before filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_filter :authenticate, :only => [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @title = @user.name
@@ -31,12 +33,19 @@ class UsersController < ApplicationController
   def update
     @user  = User.find(params[:id])
     if @user.update_attributes(params[:user])
-      redirect_to @user, :flash => {:success => "Profile updated"}
+      redirect_to @user, :flash => {:success => "Profile updated."}
     else
       @title = "Edit user"
       render 'edit'
     end
   end
+
+  private
+
+    def authenticate
+      deny_access unless signed_in?
+
+    end
 
 end
 
