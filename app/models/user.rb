68d22f6e04f580012917cad95f5911b3f50e0933
Mihause <mihause@ngs.ru>
2011-09-30 13:23:10 +0700

    Working follow/unfollow buttons (with Ajax)

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,7 +39,7 @@ class User < ActiveRecord::Base
   end
 
   def follow!(followed)
-    relationships.create!(:follower_id => followed.id)
+    relationships.create!(:followed_id => followed.id)
   end
 
   def unfollow!(followed)
